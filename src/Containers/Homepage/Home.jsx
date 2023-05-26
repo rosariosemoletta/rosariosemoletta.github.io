03d65d0f1c7665d903a7ed853d88b700387f9667
Varadon <frankshizuo07@gmail.com>
2023-05-26 14:10:20 +0200

    feat: new images, new font

--- src/Containers/Homepage/Home.jsx
+++ src/Containers/Homepage/Home.jsx
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <div className="home">
       <div className="image-container">
-        <img src="/ross.jpg" alt="me" />
+        <img src="/Foto1.jpg" alt="me" />
       </div>
       <div className="text-container">
         <div>UI Designer</div>
         <div>3D Generalist</div>
         <div>Videomaker </div>
         <div className="country">
-          From &nbsp; <div style={{ color: "red" }}> ITALY </div>
+          From &nbsp; <div style={{ color: "#e40037" }}> ITALY </div>
         </div>
       </div>
       <div className="social-container">
@@ -21,7 +21,7 @@ const Home = () => {
           rel="noreferrer"
           target="_blank"
         >
-          <img src="/linkedin-in.svg" width="30px" height="30px" alt="lnk" />
+          <img src="/linkedin-in.svg" width="45px" height="45px" alt="lnk" />
         </a>
         <a
           className="social-icon ig"
@@ -29,7 +29,7 @@ const Home = () => {
           rel="noreferrer"
           target="_blank"
         >
-          <img src="/instagram.svg" width="30px" height="30px" alt="ig" />
+          <img src="/instagram.svg" width="45px" height="45px" alt="ig" />
         </a>
         <a
           className="social-icon bhc"
@@ -39,8 +39,8 @@ const Home = () => {
         >
           <img
             src="/behance.svg"
-            width="30px"
-            height="30px"
+            width="45px"
+            height="45px"
             alt="bhc"
             target="_blank"
           />
